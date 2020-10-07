c9d740cb126225b5c90eb53b399da3452a9c63e9
Agay Borges <agay.borges@hivecloud.com.br>
2020-10-06 23:25:58 -0300

    [BEER-1] add swagger and correct urls for restful pattern

--- src/main/java/beertech/restfullapidocorm/controller/ToolApiController.java
+++ src/main/java/beertech/restfullapidocorm/controller/ToolApiController.java
@@ -1,8 +1,6 @@
 package beertech.restfullapidocorm.controller;
 
 import beertech.restfullapidocorm.service.ToolService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.istack.NotNull;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import beertech.restfullapidocorm.model.Tool;
@@ -14,70 +12,65 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.List;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-10-07T00:00:49.580Z")
 
 @Controller
+@RequestMapping("/tool")
 public class ToolApiController  {
 
     private static final Logger log = LoggerFactory.getLogger(ToolApiController.class);
-
-    private final ObjectMapper objectMapper;
 
     private final HttpServletRequest request;
 
     private final ToolService toolService;
 
     @Autowired
-    public ToolApiController(ObjectMapper objectMapper, HttpServletRequest request, ToolService toolService) {
-        this.objectMapper = objectMapper;
+    public ToolApiController(HttpServletRequest request, ToolService toolService) {
         this.request = request;
         this.toolService = toolService;
     }
 
 
-    @ApiOperation(value = "Add a new tool", nickname = "addTool", notes = "", tags={ "tool", })
+    @ApiOperation(value = "Add a new tool", nickname = "POST", notes = "", tags={ "tool", })
     @ApiResponses(value = {
             @ApiResponse(code = 405, message = "Invalid input") })
-    @RequestMapping(value = "/tool",
+    @RequestMapping(value = "/",
             produces = { "application/json" },
             consumes = { "application/json" },
             method = RequestMethod.POST)
     public ResponseEntity<Void> addTool(@ApiParam(value = "Tool object that needs to be added" ,required=true )  @RequestBody Tool body) {
-        String accept = request.getHeader("Accept");
         toolService.saveTool(body);
         return new ResponseEntity<Void>(HttpStatus.OK);
     }
 
-    @ApiOperation(value = "Deletes a tool", nickname = "deleteTool", notes = "", tags={ "tool", })
-    @ApiResponses(value = {
-            @ApiResponse(code = 400, message = "Invalid ID supplied"),
-            @ApiResponse(code = 404, message = "Tool not found") })
-    @RequestMapping(value = "/tool/findByToolId",
+    @ApiOperation(value = "Deletes a tool", nickname = "DELETE", notes = "", tags={ "tool", })
+    @ApiResponses(value = @ApiResponse(code = 400, message = "Invalid ID supplied"))
+    @RequestMapping(value = "/{toolId}",
             produces = { "application/json" },
             method = RequestMethod.DELETE)
-    public ResponseEntity<Void> deleteTool(@NotNull @ApiParam(value = "Tool id to delete", required = true) @RequestParam(value = "toolId", required = true) Long toolId) {
-        String accept = request.getHeader("Accept");
-        toolService.deleteById(toolId);
+    public ResponseEntity<Void> deleteTool(@ApiParam(value = "Tool id to delete", required = true) @PathVariable(value = "toolId") Long toolId) {
+        try {
+            toolService.deleteById(toolId);
+        } catch (Exception e) {
+            return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);    
+        }
         return new ResponseEntity<Void>(HttpStatus.OK);
     }
 
-    @ApiOperation(value = "Finds tools by id", nickname = "findByToolId", notes = "Multiple status values can be provided with comma separated strings", response = Tool.class, responseContainer = "List", tags={ "tool", })
+    @ApiOperation(value = "Finds tools by id", nickname = "GET", notes = "Multiple status values can be provided with comma separated strings", response = Tool.class, responseContainer = "List", tags={ "tool", })
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "successful operation", response = Tool.class, responseContainer = "List"),
             @ApiResponse(code = 400, message = "Invalid status value") })
-    @RequestMapping(value = "/tool/findByToolId",
+    @RequestMapping(value = "/{toolId}",
             produces = { "application/json" },
             method = RequestMethod.GET)
-    public ResponseEntity<Tool> findByToolId(@NotNull @ApiParam(value = "Status values that need to be considered for filter", required = true)  @RequestParam(value = "toolId", required = true) Long toolId) {
+    public ResponseEntity<Tool> findByToolId(@ApiParam(value = "Status values that need to be considered for filter", required = true)  @PathVariable(value = "toolId") Long toolId) {
         String accept = request.getHeader("Accept");
         if (accept != null && accept.contains("application/json")) {
             try {
@@ -90,18 +83,29 @@ public class ToolApiController  {
         return new ResponseEntity<Tool>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ApiOperation(value = "Update an existing tool", nickname = "updateTool", notes = "", tags={ "tool", })
+    @ApiOperation(value = "Update an existing tool", nickname = "PUT", notes = "", tags={ "tool", })
     @ApiResponses(value = {
             @ApiResponse(code = 400, message = "Invalid ID supplied"),
             @ApiResponse(code = 404, message = "Tool not found"),
             @ApiResponse(code = 405, message = "Validation exception") })
-    @RequestMapping(value = "/tool",
+    @RequestMapping(value = "/{toolId}",
             produces = { "application/json" },
             consumes = { "application/json" },
             method = RequestMethod.PUT)
-    public ResponseEntity<Void> updateTool(@ApiParam(value = "Tool object that needs to be added" ,required=true )  @RequestBody Tool body) {
-        String accept = request.getHeader("Accept");
-        toolService.updateTool(body);
+    public ResponseEntity<Void> updateTool(
+        @ApiParam(value = "Tool object that needs to be added" ,required=true )  @RequestBody Tool body,
+        @ApiParam(value = "Tool object that needs to be added" ,required=true ) @PathVariable(value = "toolId") Long toolId) {
+
+        if (toolId != body.getToolId()) {
+            return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            toolService.updateTool(body);
+        } catch (Exception e) {
+            return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+        }
+
         return new ResponseEntity<Void>(HttpStatus.OK);
     }
 
